ddd36502defb65567b4a367817540cc5f1db08a0
0x787af25e <74492257+0x787af25e@users.noreply.github.com>
2025-09-21 14:18:06 +0800

    Update run.sh
    Make it work in the first place

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,14 +5,14 @@ set -euo pipefail
 mkdir -p ./{data/$(date +%F),analysis/$(date +%F)}
 
 # Downloading scripts
-#wget -P scripts/ http://kirill-kryukov.com/study/tools/blast-parser/blast_parser_1.1.5.zip
-curl http://kirill-kryukov.com/study/tools/blast-parser/blast_parser_1.1.5.zip > scripts/blast_parser_1.1.5.zip
-unzip scripts/blast_parser_1.1.5.zip -d scripts
+wget -P scripts/ http://kirill-kryukov.com/study/tools/blast-parser/blast_parser_1.1.5.zip
+# curl http://kirill-kryukov.com/study/tools/blast-parser/blast_parser_1.1.5.zip > scripts/blast_parser_1.1.5.zip
+unzip -n scripts/blast_parser_1.1.5.zip -d scripts
 
 # Downloading data
-#wget -nv -P data/ http://www.mgc.ac.cn/VFs/Down/VFDB_setA_nt.fas.gz http://www.mgc.ac.cn/VFs/Down/VFDB_setA_pro.fas.gz
-curl http://www.mgc.ac.cn/VFs/Down/VFDB_setA_nt.fas.gz > data/VFDB_setA_nt.fas.gz
-curl http://www.mgc.ac.cn/VFs/Down/VFDB_setA_pro.fas.gz > data/VFDB_setA_pro.fas.gz
+wget -nv -P data/ http://www.mgc.ac.cn/VFs/Down/VFDB_setA_nt.fas.gz http://www.mgc.ac.cn/VFs/Down/VFDB_setA_pro.fas.gz
+# curl http://www.mgc.ac.cn/VFs/Down/VFDB_setA_nt.fas.gz > data/VFDB_setA_nt.fas.gz
+# curl http://www.mgc.ac.cn/VFs/Down/VFDB_setA_pro.fas.gz > data/VFDB_setA_pro.fas.gz
 
 gunzip -c data/VFDB_setA_nt.fas.gz > data/VFDB_setA_nt.fas
 gunzip -c data/VFDB_setA_pro.fas.gz > data/VFDB_setA_pro.fas
